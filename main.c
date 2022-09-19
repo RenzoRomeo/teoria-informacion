b41f408ce537ef612f1568b9ac1265a5bed9add5
Renzo Romeo <renzoromeo44@gmail.com>
2022-09-19 16:30:20 -0300

    Primera parte finalizada

--- main.c
+++ main.c
@@ -1,60 +1,21 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 #define CANT_SIMBOLOS 3
 #define CANT_CARACTERES 10000
 #define DIF 0.01
 
-void leerArchivo(char* nombreArchivo, float probabilidad[], float mat[][CANT_SIMBOLOS]) {
-    FILE *arch = fopen(nombreArchivo, "r");
-    if (arch == NULL) {
-        printf("No se pudo abrir el archivo");
-        return;
-    }
+double entropiaExtension(float probabilidades[], int orden);
 
-    char ant = fgetc(arch);
-    while (!feof(arch)) {
-        probabilidad[ant - 'A']++;
+void leerArchivo(char *nombreArchivo, float probabilidad[], float mat[][CANT_SIMBOLOS]);
 
-        char sig = fgetc(arch);
-        mat[sig - 'A'][ant - 'A']++;
+int esMemoriaNula(float probabilidades[], float mat[][CANT_SIMBOLOS]);
 
-        ant = sig;
-    }
+float calcularEntropia(float probabilidades[]);
 
-    for (int i = 0; i < CANT_SIMBOLOS; i++) {
-        probabilidad[i] /= (float)CANT_CARACTERES;
-    }
-
-    for (int i = 0; i < CANT_SIMBOLOS; i++) {
-        for (int j = 0; j < CANT_SIMBOLOS; j++) {
-            mat[i][j] /= (float)(CANT_CARACTERES - 1);
-        }
-    }
-
-    fclose(arch);
-}
-
-int esMemoriaNula(float probabilidades[], float mat[][CANT_SIMBOLOS]) {
-    for (int i = 0; i < CANT_SIMBOLOS; i++) {
-        for (int j = 0; j < CANT_SIMBOLOS; j++) {
-            if (fabs(mat[j][i] - probabilidades[i] * probabilidades[j]) > DIF)
-                return 0;
-        }
-    }
-    return 1;
-}
-
-float calcularEntropia(float probabilidades[]) {
-    float entropia = 0;
-    for (int i = 0; i < CANT_SIMBOLOS; i++) {
-        float prob = probabilidades[i];
-        entropia += prob * (log(prob) / log(2.0));
-    }
-    return -entropia;
-}
-
-int main() {
+int main()
+{
     float probabilidades[CANT_SIMBOLOS];
     float mat[CANT_SIMBOLOS][CANT_SIMBOLOS] = {0};
 
@@ -67,8 +28,10 @@ int main() {
     printf("\n");
 
     printf("Matriz de transicion: \n");
-    for (int i = 0; i < CANT_SIMBOLOS; i++) {
-        for (int j = 0; j < CANT_SIMBOLOS; j++) {
+    for (int i = 0; i < CANT_SIMBOLOS; i++)
+    {
+        for (int j = 0; j < CANT_SIMBOLOS; j++)
+        {
             printf("%f ", mat[i][j]);
         }
         printf("\n");
@@ -80,5 +43,109 @@ int main() {
     // Inciso B
     float entropia = calcularEntropia(probabilidades);
     printf("Entropia (Fuente Original): %f bits\n", entropia);
-    printf("Entropia (Extension Orden 20): %f bits\n", entropia * 20);
+    printf("Entropia (Extension Orden 20, n * H(S)): %f bits\n", entropia * 20);
+    printf("Entropia (Extension Orden 20, calculada por extension): %f bits\n", entropiaExtension(probabilidades, 20));
+}
+
+double entropiaExtension(float probabilidades[], int orden)
+{
+    char *extension = (char *)malloc(sizeof(char) * orden);
+
+    for (int i = 0; i < orden; i++)
+    {
+        extension[i] = 0;
+    }
+
+    unsigned int cantCiclos = (unsigned int)pow(CANT_SIMBOLOS, orden);
+
+    double entropiaOrdenN = 0.0;
+
+    // Para cada extension
+    for (unsigned int i = 0; i < cantCiclos; i++)
+    {
+        double probabilidadExtension = 1.0;
+        for (int j = 0; j < orden; j++)
+        {
+            probabilidadExtension *= probabilidades[extension[j]];
+        }
+        entropiaOrdenN += probabilidadExtension * (-log2(probabilidadExtension));
+
+        // Incremento de la extension
+        for (int j = 0; j < orden; j++)
+        {
+            if (extension[j] < CANT_SIMBOLOS - 1)
+            {
+                extension[j]++;
+                break;
+            }
+            else
+            {
+                extension[j] = 0;
+            }
+        }
+    }
+
+    free(extension);
+
+    return entropiaOrdenN;
+}
+
+void leerArchivo(char *nombreArchivo, float probabilidad[], float mat[][CANT_SIMBOLOS])
+{
+    FILE *arch = fopen(nombreArchivo, "r");
+    if (arch == NULL)
+    {
+        printf("No se pudo abrir el archivo");
+        return;
+    }
+
+    char ant = fgetc(arch);
+    while (!feof(arch))
+    {
+        probabilidad[ant - 'A']++;
+
+        char sig = fgetc(arch);
+        mat[sig - 'A'][ant - 'A']++;
+
+        ant = sig;
+    }
+
+    for (int i = 0; i < CANT_SIMBOLOS; i++)
+    {
+        probabilidad[i] /= (float)CANT_CARACTERES;
+    }
+
+    for (int i = 0; i < CANT_SIMBOLOS; i++)
+    {
+        for (int j = 0; j < CANT_SIMBOLOS; j++)
+        {
+            mat[i][j] /= (float)(CANT_CARACTERES - 1);
+        }
+    }
+
+    fclose(arch);
+}
+
+int esMemoriaNula(float probabilidades[], float mat[][CANT_SIMBOLOS])
+{
+    for (int i = 0; i < CANT_SIMBOLOS; i++)
+    {
+        for (int j = 0; j < CANT_SIMBOLOS; j++)
+        {
+            if (fabs(mat[j][i] - probabilidades[i] * probabilidades[j]) > DIF)
+                return 0;
+        }
+    }
+    return 1;
+}
+
+float calcularEntropia(float probabilidades[])
+{
+    float entropia = 0;
+    for (int i = 0; i < CANT_SIMBOLOS; i++)
+    {
+        float prob = probabilidades[i];
+        entropia += prob * (log(prob) / log(2.0));
+    }
+    return -entropia;
 }
