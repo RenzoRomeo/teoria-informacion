bf8fc9eb765e0685f492dd2099d7b1b7d070640b
Renzo Romeo <renzoromeo44@gmail.com>
2022-10-19 16:39:48 -0300

    Probabilidades se inicializa en 0.

--- main.c
+++ main.c
@@ -1,20 +1,23 @@
 #include "parte1.c"
 #include "parte2.c"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     FILE *resultados = fopen("parte1_incisoA.txt", "w");
 
     int calcularEntropiaOrden20 = 0;
-    
+
     // Buscar flag -o
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-o") == 0) {
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-o") == 0)
+        {
             calcularEntropiaOrden20 = 1;
         }
     }
 
-    // PARTE 1   
-    double probabilidades[CANT_SIMBOLOS];
+    // PARTE 1
+    double probabilidades[CANT_SIMBOLOS] = {0};
     double mat[CANT_SIMBOLOS][CANT_SIMBOLOS] = {0};
 
     // Inciso A
@@ -45,19 +48,19 @@ int main(int argc, char *argv[]) {
     double entropia = calcularEntropia(probabilidades);
     fprintf(resultados, "Entropia (Fuente Original): %f\n", entropia);
     fprintf(resultados, "Entropia (Extension Orden 20, n * H(S)): %f\n", entropia * 20);
-    
-    if(calcularEntropiaOrden20)
+
+    if (calcularEntropiaOrden20)
     {
         fprintf(resultados, "Entropia (Extension Orden 20, calculada por extension): %f\n", entropiaExtension(probabilidades, 20));
     }
 
     fclose(resultados);
-    
+
     // PARTE 2
     procesarCodigo(3);
     procesarCodigo(5);
     procesarCodigo(7);
-    
+
     printf("Programa ejecutado con exito \n");
 
     return 0;
