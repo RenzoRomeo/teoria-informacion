09cd4c518f94bd50c0c168a9846b13e3459207ee
Renzo Romeo <renzoromeo44@gmail.com>
2022-11-21 11:43:00 -0300

    Propiedades almacenadas en archivos de salida.

--- src/codigo/Principal.java
+++ src/codigo/Principal.java
@@ -7,13 +7,32 @@ public class Principal {
     public static void main(String[] args) throws IOException {
         TreeMap<String, Double> probabilidades = ParteUno.leerArchivo("datos.txt");
 
+        if (!new java.io.File("salidas").exists()) {
+            new java.io.File("salidas").mkdir();
+        }
+
+        // El valor de 71 símbolos para el alfabeto del texto original
+        // está hardcodeado porque tuvimos que contar a mano el número de
+        // símbolos que aparecen en el texto original, ya que en el archivo
+        // las palabras con tilde están codificadas como 2 caracteres.
+        ParteUno.propiedades("propiedadesOriginal.txt", probabilidades, 71);
+
         TreeMap<String, String> codigosHuffman = Huffman.huffman(probabilidades);
+
+        TreeMap<String, Double> probabilidadesHuffman = ParteUno.probabilidadesCodigos(probabilidades, codigosHuffman);
+
+        ParteUno.propiedades("propiedadesHuffman.txt", probabilidadesHuffman, 2);
+
         TreeMap<String, String> codigosShannon = ShannonFano.shannonFano(probabilidades);
 
-        ParteUno.comprimir("datos.txt", "comprimido.huff", codigosHuffman);
-        ParteUno.comprimir("datos.txt", "comprimido.shan", codigosShannon);
+        TreeMap<String, Double> probabilidadesShannon = ParteUno.probabilidadesCodigos(probabilidades, codigosShannon);
 
-        ParteUno.descomprimir("comprimido.huff", "descomprimidoHuffman.txt");
-        ParteUno.descomprimir("comprimido.shan", "descomprimidoShannon.txt");
+        ParteUno.propiedades("propiedadesShannon.txt", probabilidadesShannon, 2);
+
+        ParteUno.comprimir("datos.txt", "comprimido.Huf", codigosHuffman);
+        ParteUno.comprimir("datos.txt", "comprimido.Fan", codigosShannon);
+
+        ParteUno.descomprimir("comprimido.Huf", "descomprimidoHuffman.txt");
+        ParteUno.descomprimir("comprimido.Fan", "descomprimidoShannon.txt");
     }
 }
